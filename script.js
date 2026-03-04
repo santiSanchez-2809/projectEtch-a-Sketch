d719c77ba2791bb794730b72a26e0a8abaa60db2
santiSanchez-2809 <santiagosanchezaimo@gmail.com>
2026-03-03 23:56:08 -0300

    Add styles to html, body, .container, .square, .header and #gridConfiguration in styles.css. Add generateGrid(rows, columns) and griConfiguration Button behaviour in script.js

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-const container = document.querySelector(".container");
+function generateGrid(rows, columns){
+    const container = document.querySelector(".container");
 
-for(let i = 0; i < 256; i++){
-    let div = document.createElement("div"); 
-    div.classList.add('square'); 
- 
-    container.appendChild(div); 
+    container.innerHTML = "";
+
+    for(let i = 0; i < rows * columns; i++){
+        let div = document.createElement("div"); 
+        div.classList.add('square'); 
+    
+        div.style.flexBasis = `calc(100% / ${columns})`; 
+        div.style.height = `calc(100% / ${rows})`; 
+
+        container.appendChild(div); 
+    }
+
+    document.querySelectorAll(".square").forEach(square =>{
+    square.addEventListener("mouseenter", (e) => {
+        e.target.style.backgroundColor = "grey";
+    })
+})
+
 }
+
+generateGrid(16, 16);
+
+
+let gridConfigurationButton = document.querySelector("#gridConfiguration");
+gridConfigurationButton.addEventListener("click", () =>{
+    let rows = Math.max(Math.min(prompt("Enter the number of rows", 16), 100), 16); 
+    let columns = Math.max(Math.min(prompt("Enter the number of rows", 16), 100), 16);
+
+    generateGrid(rows, columns);
+})
